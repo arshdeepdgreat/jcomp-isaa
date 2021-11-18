6e6e6247dc3fac76620d549914390e247dad09ff
arshdeepdgreat <arshdeepdgreat@gmail.com>
2021-11-18 10:52:10 +0530

    unnessary file del

--- bpcs/bpcs_steg_encode.py
+++ bpcs/bpcs_steg_encode.py
@@ -8,15 +8,6 @@ from .bpcs_steg import arr_bpcs_complexity, conjugate
 
 ALIVE, CONJUGATING, DEAD = 0,1,2
 def get_message_and_status(message, dims, conjugated, status, alpha):
-    """
-    message is remaining message to be embedded
-    dims is shape of next desired grid
-    conjugated is list of bool, specifying whether each past message grid was conjugated or not
-    status is int
-        DEAD means we shouldn't be embedding any more messages
-        ALIVE means we need the next grid from message
-        CONJUGATING means we need the next grid from conjugated
-    """
     if status == DEAD:
         return None, None, None, DEAD
     elif status == CONJUGATING and len(conjugated) == 0:
@@ -30,12 +21,6 @@ def get_message_and_status(message, dims, conjugated, status, alpha):
         return cur, message, conjugated, ALIVE
 
 def embed_message_in_vessel(arr, alpha, message, grid_size):
-    """
-    want all o.g. grids to have complexity < alpha
-    if you find one greater than alpha
-    replace it with message
-    if message complexity < alpha, conjugate message
-    """
     conjugated = []
     status = ALIVE
     nmessgs, nmaps, nleft, ngrids = 0, 0, 0, 0
@@ -47,14 +32,13 @@ def embed_message_in_vessel(arr, alpha, message, grid_size):
             continue
         cur_message, message, conjugated, status = get_message_and_status(message, grid.shape, conjugated, status, alpha)
         if status == DEAD:
-            # since there is no more embedding to do, flip the remaining grids you find
-            # so that they are not mistaken for signal grids when decoding
             cur_message = np.zeros(grid.shape, dtype=np.uint8)
             if not arr_bpcs_complexity(cur_message) < alpha:
                 a = arr_bpcs_complexity(grid)
                 b = arr_bpcs_complexity(cur_message)
                 raise Exception('Error fixing vessel grid to have complexity below alpha: {0} => {1}'.format(a, b))
             nleft += 1
+
         if status == ALIVE and arr_bpcs_complexity(cur_message) < alpha:
             cur_message = conjugate(cur_message)
             if not arr_bpcs_complexity(cur_message) >= alpha:
@@ -63,6 +47,7 @@ def embed_message_in_vessel(arr, alpha, message, grid_size):
                 raise Exception('Error fixing message grid to have complexity above alpha: {0} => {1}'.format(a, b))
             nmessgs += 1
             conjugated.append(True)
+
         elif status == ALIVE:
             nmessgs += 1
             conjugated.append(False)
@@ -70,6 +55,7 @@ def embed_message_in_vessel(arr, alpha, message, grid_size):
             nmaps += 1
         assert cur_message.shape == grid.shape
         arr[tuple(dims)] = cur_message
+        
     if message is not None and message.size > 0:
         raise Exception("Could not fit message in arr. Still had {0} bits left".format(message.size))
     elif status != DEAD:
